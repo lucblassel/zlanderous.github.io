a5eff81e9216ddb3af245c12fb16a4695944baaa
Luc Blassel <luc.blassel@gmail.com>
2020-03-12 15:26:31 +0100

    testing javascript stuff

---
+++ _drafts/javascript_test.md
@@ -0,0 +1,24 @@
+---
+layout: post
+title: Javscript testing ?
+mathjax: true
+excerpt_separator: <!--more-->
+tags: [linear model, machine-learning, Python, code, regression]
+comments: true
+feature: https://upload.wikimedia.org/wikipedia/commons/e/ed/Residuals_for_Linear_Regression_Fit.png
+align: true
+---
+
+This is a test see what type of javascipt I can include in my file...
+
+<!--more-->
+
+## Test
+
+<canvas id="chart"></canvas>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min.js"></script>
+<script src="https://gmousse.github.io/dataframe-js/dist/dataframe.min.js"></script>
+<script src="{{site.baseurl}}/assets/js/papaparse.min.js"></script>
+<script src="{{site.baseurl}}/assets/js/js_test.js"></script>
--- assets/js/js_test.js
+++ assets/js/js_test.js
@@ -1,8 +1,40 @@
-let sketch = function(p) {
-    p.setup = function(){
-      p.createCanvas(100, 100);
-      p.background(0);
-      console.log("instanced")
+
+var DataFrame = dfjs.DataFrame
+
+let filename = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+
+
+DataFrame.fromCSV(filename)
+    .then(makeChart);
+
+//d3.csv(filename)
+//    .then(makeChart);
+
+function makeChart(dataframe) {
+  filterData(dataframe);
+  var chart = new Chart('chart', {
+    type: 'horizontalBar',
+    data: {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [10, 20, 30]
+      	}
+      ]
     }
-  };
-new p5(sketch, window.document.getElementById('myCanvas'));
+  });
+}
+
+function filterData(dataframe) {
+  var cols = dataframe.listColumns();
+  for (i=0; i<4; i++) {
+    cols.shift();
+  }
+  var dfArray = dataframe.groupBy('Country/Region').toCollection();
+  for (group of dfArray) {
+   // console.log(group);
+    if (group['groupKey']['Country/Region'] == 'France') {
+      console.log(group['group']['matrix']);
+    }
+  }
+}
